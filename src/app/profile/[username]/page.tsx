c51e27a5e69fe89925dced0ed2b166ef8a60e0d2
unknown <omar.hosny@nasserpharmacy.net>
2025-03-21 23:50:40 +0200

    fix issue in profile page params by adding it's type

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -4,14 +4,23 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async ({ params }: { params: { username: string } }) => {
-  const user = await getUserByUsername(params.username as string);
+type Params = Promise<{
+  username: string;
+}>;
+const ProfilePage = async ({ params }: { params: Params }) => {
+  // Directly use the username from params without any await
+  const { username } = await params;
+
+  console.log("Resolved username:", username); // Debugging log
+
+  const user = await getUserByUsername(username);
   if (!user) return null;
+
   return (
-    <div className="w-full flex flex-col items-center ">
+    <div className="w-full flex flex-col items-center">
       <div className="w-full flex flex-col items-center bg-neutral-50 dark:bg-transparent rounded-2xl p-2 max-w-[750px]">
-        {/* avatar */}
-        <div className=" rounded-full overflow-hidden">
+        {/* Avatar */}
+        <div className="rounded-full overflow-hidden">
           <Image
             src={user.image || "/profile.png"}
             alt={user.name}
@@ -20,7 +29,7 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
             className="rounded-full"
           />
         </div>
-        {/* name */}
+        {/* Name */}
         <div className="flex flex-col items-center gap-1">
           <Link href={`/profile/${user.username}`}>
             <p>{user.name}</p>
@@ -29,12 +38,12 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
             @{user.username}
           </p>
         </div>
-        {/* bio */}
+        {/* Bio */}
         <div className="text-center text-gray-500 text-sm mt-1">
           {user.bio || "No bio"}
         </div>
-        {/* followers / following */}
-        <div className="flex items-center justify-around gap-2 mt-2 w-5/6 max-w-xl dark:text-gray-200  text-gray-500 ">
+        {/* Followers / Following */}
+        <div className="flex items-center justify-around gap-2 mt-2 w-5/6 max-w-xl dark:text-gray-200 text-gray-500">
           <p className="text-sm">{user._count.followers} followers</p>
           <p className="text-sm">{user._count.following} following</p>
         </div>
@@ -44,8 +53,8 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
       <div className="text-2xl pt-2 text-neutral-500">
         <h2>Your Posts</h2>
       </div>
-      {/* posts */}
-      <Posts username={params.username as string} />
+      {/* Posts */}
+      <Posts username={username} />
     </div>
   );
 };
